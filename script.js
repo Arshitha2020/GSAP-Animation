1d2cd98083d3ad8dcc45afac22914e5ace809371
Arshitha2020 <arshitha.s.a@sap.com>
2022-02-06 19:44:25 +0530

    First draft

---
+++ script.js
@@ -0,0 +1,15 @@
+gsap.from(".header", { duration: 2, y: "-100%", ease: "bounce" });
+gsap.from(".link", {
+  duration: 2,
+  opacity: 0,
+  delay: 1,
+  stagger: 0.5,
+  repeat: -1,
+});
+gsap.from("#Laptop", { duration: 1.5, y: "-100%", ease: "bounce", repeat: -1 });
+gsap.from("#Head", { duration: 1.5, y: "-100%", ease: "bounce", repeat: -1 });
+gsap.from("#Body", { duration: 1.5, y: "-100%", ease: "bounce", repeat: -1 });
+gsap.from("#Pants", { duration: 1.5, y: "-100%", ease: "bounce", repeat: -1 });
+gsap.from("#Vase", { duration: 1.5, y: "-100%", ease: "bounce", repeat: -1 });
+gsap.from("#Pot", { duration: 1.5, y: "-100%", ease: "bounce", repeat: -1 });
+gsap.from("#Shoes", { duration: 1.5, y: "-100%", ease: "bounce", repeat: -1 });
